913cb579cc2e468ab86202a8f27e16dbc5f7199c
Carmen Garcia Alonso <carmenga24teis@gmail.com>
2025-09-29 12:26:44 +0200

    siguiendo profe(?)

---
+++ Jigglypuff/index.js
@@ -0,0 +1,22 @@
+//let pokemon = "jigglypuff";
+const startList = document.getElementById("stats");
+const ruido = document.getElementById("ruido");
+const input = document.getElementById("pokemonInput")
+const btn = document.getElementById("buscarBtn");
+
+btn.addEventListener("click", () => {
+    const pokemon = input.value.trim().toLowerCase();
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((response) => response.json())
+    .then((data) => {
+        const stats = data.stats;
+        stats.forEach((stat) => {
+            const statLi = document.createElement("li");
+            statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+            statsList.appendChild(statLi);
+        });
+    })
+    .catch((error) => console.error("Error al obtener los datos:", error));
+})
+
